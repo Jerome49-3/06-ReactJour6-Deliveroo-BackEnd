82eb4d1e71b883a636080c7fe7c2e9238f048f07
jerome <contact@teilienCard.com>
2025-08-01 23:45:35 +0200

    change CORS

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: process.env.URL_CORS_FRONTEND,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 //*************** MONGOOSE ***************/
 const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGODB_URI);
